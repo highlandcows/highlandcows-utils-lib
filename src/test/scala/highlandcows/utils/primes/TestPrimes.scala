65f8da2b468a5493db96ea693f62b4a303f07fe3
njacobs5074 <nick@highlandcows.com>
2022-02-19 17:30:49 +0200

    Scalafix issue fixed

--- src/test/scala/highlandcows/utils/primes/TestPrimes.scala
+++ src/test/scala/highlandcows/utils/primes/TestPrimes.scala
@@ -3,9 +3,10 @@ package highlandcows.utils.primes
 import org.scalatest.flatspec.AnyFlatSpec
 
 class TestPrimes extends AnyFlatSpec {
-  val calculators =
+  val calculators: Seq[Calculator] =
     Seq(Calculator(Calculator.TEXTBOOK), Calculator(Calculator.GUILDENSTERN70), Calculator(Calculator.RECURSIVE))
-  val basicPrimes = Seq(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97)
+  val basicPrimes: Seq[Int] =
+    Seq(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97)
 
   "Simple primes test" should "identify primes correctly" in {
     calculators.foreach(calculator => basicPrimes.foreach(prime => assert(calculator.isPrime(prime), prime.toString)))
